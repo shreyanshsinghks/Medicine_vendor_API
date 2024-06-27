2463c7520847b4c0a809266c21633852486b9129
Shreyansh <shreyanshsinghks@gmail.com>
2024-06-27 23:41:30 +0530

    Implemented on python anywhere 27 June. Order functionality added. All checked working status

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,9 @@
 from arrow import get
 from flask import Flask, render_template, request, jsonify
 
-from create import createTables, createUser
-from GetUsers import getAllusers, getSpecificUsers, getAllProducts
-from UserOperation import updateUserAccess
+from create import createTables, createUser, createOrder
+from GetUsers import getAllusers, getSpecificUsers, getAllProducts, getAllOrders
+from UserOperation import updateUserAccess, updateOrderAccess
 
 # Adding extra
 from create import createProduct
@@ -42,6 +42,18 @@ def create_product():
     else:
         return jsonify({"message":"failed", "status":400})
     
+# Adding the code for inserting order
+@app.route('/createOrder',methods = ['POST'])
+def create_order():
+    itemId =  request.form['itemId']
+    quantity = request.form['quantity']
+    venderId = request.form['venderId']
+    dbReb = createOrder(itemId=itemId, quantity=quantity, venderId=venderId)
+    if dbReb:
+        return jsonify({"message":"success", "status":200})
+    else:
+        return jsonify({"message":"failed", "status":400})
+    
 # Adding the code for getting all the products
 @app.route('/getAllProducts',methods = ['GET'])
 def get_all_products():
@@ -52,6 +64,12 @@ def get_all_products():
 @app.route('/getAllUsers',methods = ['GET'])
 def getAllUsers():
     return getAllusers()
+
+# Adding the code for getting all the orders
+@app.route('/getAllOrders',methods = ['GET'])
+def get_all_orders():
+    orders = getAllOrders()
+    return orders
 
 @app.route('/getSpecificUser',methods = ['GET'])
 def getSpecificUser():
@@ -65,6 +83,14 @@ def update_user_access():
     blocked = request.form['blocked']
     updateUserAccess(id = userId,approved = approved,blocked = blocked)
     return "Access updated successfully"
+
+# Updating order isApproved status
+@app.route('/updateOrderAccess',methods = ['PATCH'])
+def update_order_access():
+    orderId = request.form['orderId']
+    approved = request.form['approved']
+    updateOrderAccess(id = orderId,approved = approved)
+    return "Order status updated successfully"
 
 
 @app.route('/', methods=['GET'])
